e5e02e1ed78ab16aabad10a84108a43567a63d94
Laure Inturrisi <83076558+laureintu@users.noreply.github.com>
2024-04-25 19:50:22 +0200

    Update python_execute.py

--- python_execute.py
+++ python_execute.py
@@ -4,7 +4,7 @@ import tqdm
 time_max=0
 for i in tqdm.trange(1000):
         #out=subprocess.check_output("time ./ipsa_sched.c",shell=True)
-        os.system("/usr/bin/time --output=outtime -p sh -c './BSS > /dev/null'")
+        os.system("/usr/bin/time --output=outtime -p sh -c './BSS > /dev/null'") #./BSS étant le fichier correspondant à la tache réalisant un binary search
         file=open("outtime","r")
         file.seek(0)
         f=file.readline()
